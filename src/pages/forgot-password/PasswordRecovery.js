af82d6faf21eb318c4ffd38d0f0010c700157991
satvikkalyan <satvikkalyan@gmail.com>
2024-03-27 18:11:34 -0400

    Refactored into components

--- src/pages/forgot-password/PasswordRecovery.js
+++ src/pages/forgot-password/PasswordRecovery.js
@@ -2,6 +2,67 @@ import React, { useState } from 'react'
 import "./password-recovery.css"
 import { Button, TextField } from '@mui/material'
 
+function EmailInputComponent({ email, setEmail, sendOTP }) {
+  return (
+    <>
+      <TextField 
+        type="email" 
+        name="email-input" 
+        id="email-input" 
+        label="Email" 
+        variant="outlined" 
+        value={email} 
+        onChange={(e) => setEmail(e.target.value)} 
+      />
+      <Button variant="contained" onClick={sendOTP}>Send OTP</Button>
+    </>
+  );
+}
+
+function OTPInputComponent({ otp, setOTP, validateOTP }) {
+  return (
+    <>
+      <TextField 
+        id="otp-input" 
+        name="otp-input" 
+        label="OTP" 
+        type="number" 
+        variant="outlined" 
+        onChange={(e) => setOTP(e.target.value)} 
+        value={otp} 
+      />
+      <Button variant="contained" onClick={validateOTP}>Confirm</Button>
+    </>
+  );
+}
+
+function PasswordResetComponent({ passwords, setPasswords, handlePasswordChange }) {
+  return (
+    <>
+      <TextField 
+        id="password-input" 
+        name="password-input" 
+        label="Password" 
+        type="password" 
+        variant="outlined" 
+        onChange={(e) => setPasswords((prev) => ({...prev, password: e.target.value}))} 
+        value={passwords.password} 
+      />
+      <TextField 
+        id="confirm-password-input" 
+        name="confirm-password-input" 
+        label="Confirm Password" 
+        type="password" 
+        variant="outlined" 
+        onChange={(e) => setPasswords((prev) => ({...prev, confirmPassword: e.target.value}))} 
+        value={passwords.confirmPassword} 
+      />
+      <Button variant="contained" onClick={handlePasswordChange}>Confirm</Button>
+    </>
+  );
+}
+
+
 function PasswordRecovery() {
   const [requestedOTP, setRequestedOTP] = useState(false)
   const [displayResetForm, setDisplayResetForm] = useState(false)
@@ -11,9 +72,9 @@ function PasswordRecovery() {
     password: "",
     confirmPassword: ""
   })
+
   const handleChange = (event) => {
-    const targetName = event.target.name
-    const targetValue = event.target.value
+    const {targetName, targetValue} = event.target
     switch (targetName) {
       case "otp-input":
         setOTP(targetValue)
@@ -62,26 +123,20 @@ function PasswordRecovery() {
   return (
     <div className='pr-main-div'>
       <div className='pr-reset-form'>
-        {displayResetForm ?
-          <>
-            <TextField id="password-input" name="password-input" label="Password" type="password" variant="outlined" onChange={handleChange} value={passwords.password} />
-            <TextField id="confirm-password-input" name="confirm-password-input" label="Confirm Password" type="password" variant="outlined" onChange={handleChange} value={passwords.confirmPassword} />
-            <Button variant="contained" onClick={handlePasswordChange} >Confirm</Button>
-          </> :
-          (requestedOTP ?
-            <>
-              <TextField id="otp-input" name="otp-input" label="OTP" type="number" variant="outlined" onChange={handleChange} value={otp} />
-              <Button variant="contained" onClick={validateOTP} >Confirm</Button>
-            </> :
-            <>
-              <TextField type="email" name="email-input" id="email-input" label="Email" variant="outlined" value={email} onChange={handleChange} />
-              <Button variant="contained" onClick={sendOTP}>Send OTP</Button>
-            </>)
-
-        }
+        {displayResetForm ? (
+          <PasswordResetComponent 
+            passwords={passwords} 
+            setPasswords={setPasswords} 
+            handlePasswordChange={handlePasswordChange} 
+          />
+        ) : requestedOTP ? (
+          <OTPInputComponent otp={otp} setOTP={setOTP} validateOTP={validateOTP} />
+        ) : (
+          <EmailInputComponent email={email} setEmail={setEmail} sendOTP={sendOTP} />
+        )}
       </div>
-
     </div>
+
   )
 }
 
